53ee4d7d038ce2712c24ef872999a33143e41b44
Angus Kellett <anguskellett@hotmail.com>
2020-06-23 22:04:49 +0000

    Use an if statement to select a school

---
+++ whichSchool.js
@@ -0,0 +1,10 @@
+const whichSchool = function (age) {
+  if 
+};
+
+console.log("I am 11. Which school should I go to?");
+console.log(whichSchool(11));
+console.log("I am 16. Which school should I go to?");
+console.log(whichSchool(16));
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
