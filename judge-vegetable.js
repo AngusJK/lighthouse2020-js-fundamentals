4601a0499258e9ab5ce8f092e2ef5f1da8d83a96
Angus Kellett <anguskellett@hotmail.com>
2020-07-08 23:18:39 +0000

    selects the winner of a gardening competition

---
+++ judge-vegetable.js
@@ -0,0 +1,27 @@
+const judgeVegetable = function (vegetables, metric) {
+  var winner = [];
+  winner.push(vegetables[0]);
+  console.log(winner);
+};
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+judgeVegetable(vegetables, metric)
