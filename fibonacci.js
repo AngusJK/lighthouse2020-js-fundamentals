a1eb6efd7ec430a2b245ba3a8062d628d3ba628f
Angus Kellett <anguskellett@hotmail.com>
2020-11-10 06:17:50 +0000

    formats with eslint

--- fibonacci.js
+++ fibonacci.js
@@ -1,22 +1,20 @@
 // Tells you whether or not a chosen number falls within the fibonacci sequence
 
-const choose = 144;
+let fibonacci = [1,2];
 
-let fibonacci = [1,2]
-
-var isFib = function(num) {
-  while (fibonacci[fibonacci.length-1] <= num) {
-    var next = fibonacci[fibonacci.length-1] + fibonacci[fibonacci.length-2];
+let isFib = function(num) {
+  while (fibonacci[fibonacci.length - 1] <= num) {
+    let next = fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
     fibonacci.push(next);
-  } 
-if (num === fibonacci[fibonacci.length-2]) {
-    console.log("Congratulations! " + num + " is part of the Fibonacci sequence!");
-  } else {
-    console.log("Sorry. " + num + " is not part of the Fibonacci sequence. Try again.");
   }
-} 
+  if (num === fibonacci[fibonacci.length - 2]) {
+    return "Congratulations! " + num + " is part of the Fibonacci sequence!";
+  } else {
+    return "Sorry. " + num + " is not part of the Fibonacci sequence. Try again.";
+  }
+};
 
-isFib(choose);
+console.log(isFib(2549));
 
 
 
