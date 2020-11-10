ae5e61654a5618f9df36499a084156b9714fbbb5
Angus Kellett <anguskellett@hotmail.com>
2020-11-10 06:20:02 +0000

    formats with eslint

--- prime.js
+++ prime.js
@@ -1,17 +1,17 @@
 // Input a number, find out if it is prime or not
 
-const isPrime = function (num) {
-  var inst = 0;
-  for (var i = 2; i < num; i++) {
+const isPrime = function(num) {
+  let inst = 0;
+  for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       inst += 1;
     }
   }
-    if (inst > 0) {
-      console.log(num + " is not a prime number.");
-    } else {
-      console.log(num + " is a prime number.");
-    }
-}
+  if (inst > 0) {
+    return num + " is NOT a prime number.";
+  } else {
+    return num + " is a prime number.";
+  }
+};
 
-isPrime(371);
+console.log(isPrime(2932));
